c52954c8d7018d63afc77f69ed11d1aa0ed3ddb9
Aly-812 <alice.casini.93@hotmail.it>
2025-04-11 20:23:12 +0200

    Compltati i primi tre punti del modulo 3

--- progetto/modulo2.py
+++ progetto/modulo2.py
@@ -8,34 +8,64 @@ def leggi_da_file(filepath):
     except FileNotFoundError:
         return None
 
-def elabora_dati_json(dati):
-    """
-    ISTRUZIONI:
-    Questa funzione prende una lista di dizionari (letto da un file JSON)
-    e dovrebbe eseguire alcune operazioni sui dati, gestendo anche eventuali
-    voci non valide o incomplete.
+def elabora_dati_json1(dati):
+    return len(dati)
 
-    Da fare in questa funzione:
+def elabora_dati_json2(dati):
+  
+    residenti_Roma = [persona["nome"] for persona in dati if persona.get("citta") == "Roma"]
+    return residenti_Roma
 
-    TODO 1. Stampa il numero totale di voci (dizionari) presenti nei dati.
-    TODO 2. Crea una nuova lista contenente solo i nomi delle persone che vivono a Roma.
-       Gestisci il caso in cui la chiave 'citta' potrebbe non essere presente.
-    TODO 3. Calcola l'età media di tutte le persone valide nei dati. Considera valide
-       solo le voci che hanno una chiave 'eta' con un valore numerico intero.
-       Ignora le voci con 'eta' mancante, non numerica o null.
-    TODO 4. Stampa un messaggio che elenca i nomi trovati nel dizionario con dati non validi 
-       o mancanti 
+def elabora_dati_json3(dati):
+    eta_persone = []
+    for persona in dati:
+        try:
+            eta_num = float(persona.get("eta"))
+            eta_persone.append(eta_num)
+        except:
+           pass
+    return sum(eta_persone) / len(eta_persone)
+      
+        
+       
 
-    Parametri:
-    - dati: Una lista di dizionari, dove ogni dizionario rappresenta una persona
-             con (idealmente) le chiavi 'nome', 'eta' e 'citta'.
+    
+            
 
-    Restituisce:
-    - None (la funzione stampa direttamente i risultati).
-    """
-    pass
+
+    
+# """
+#     ISTRUZIONI:
+#     Questa funzione prende una lista di dizionari (letto da un file JSON)
+#     e dovrebbe eseguire alcune operazioni sui dati, gestendo anche eventuali
+#     voci non valide o incomplete.
+
+#     Da fare in questa funzione:
+
+#     TODO 1. Stampa il numero totale di voci (dizionari) presenti nei dati.
+    
+#     TODO 2. Crea una nuova lista contenente solo i nomi delle persone che vivono a Roma.
+#        Gestisci il caso in cui la chiave 'citta' potrebbe non essere presente.
+    
+#     TODO 3. Calcola l'età media di tutte le persone valide nei dati. Considera valide
+#        solo le voci che hanno una chiave 'eta' con un valore numerico intero.
+#        Ignora le voci con 'eta' mancante, non numerica o null.
+#     TODO 4. Stampa un messaggio che elenca i nomi trovati nel dizionario con dati non validi 
+#        o mancanti 
+
+#     Parametri:
+#     - dati: Una lista di dizionari, dove ogni dizionario rappresenta una persona
+#              con (idealmente) le chiavi 'nome', 'eta' e 'citta'.
+
+#     Restituisce:
+#     - None (la funzione stampa direttamente i risultati).
+# """
 
 if __name__ == "__main__":
     # Codice per provare la funzione "elabora_dati_json"
-    pass
+    dizionario = leggi_da_file("dati/test.json")
+    print(elabora_dati_json1(dizionario))
+    print(elabora_dati_json2(dizionario))
+    print(elabora_dati_json3(dizionario))
+    
 
