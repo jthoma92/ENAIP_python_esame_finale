93d9362c9210c77faefe5231f9cb01aad1eb6806
jthoma92 <jthoma92@gmail.com>
2025-04-17 12:27:13 +0200

    Modulo 2 Aggiornato con codice

--- progetto/modulo2.py
+++ progetto/modulo2.py
@@ -5,7 +5,8 @@ def leggi_da_file(filepath):
     try:
         with open(filepath, 'r') as f:
             return json.load(f)
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        print(e)
         return None
 
 def elabora_dati_json(dati):
@@ -33,9 +34,74 @@ def elabora_dati_json(dati):
     Restituisce:
     - None (la funzione stampa direttamente i risultati).
     """
-    pass
+    print(f"\nVoci totali presenti nel dizionario: {len(dati)}\n")
 
+    lista_roma = []
+
+    for persona in dati:
+        if "citta" in persona:
+            if persona["citta"].lower() == "roma":
+                lista_roma.append(persona["nome"])
+
+    print(f"Le persone che vivono a Roma:")
+    for persona in lista_roma:
+        print(persona)
+    
+    eta = 0
+    count = 0
+
+    for persona in dati:
+        if "eta" in persona:
+            if type(persona["eta"]) == int:
+                eta += persona["eta"]
+                count += 1
+
+    print(f"\nL'età media è: {eta/count:.2f}\n") 
+
+    lista_sbagliati = []
+    lista_criteri = ["eta", "nome", "citta", "SBAGLIATO"] #definizione dei criteri di una persona per il seguente Ciclo for (include anche la nuova chiave "SBAGLIATO")
+
+    #Questo Ciclo For controlla ogni dizionario del file JSON Per errori, aggiungendo una chiave ai eventuali dizonario problematici
+    #La chiave si chiama "SBAGLIATO" e contiene una lista descrittiva del problema (o dei problemi), e viene aggiunta all'invizio
+    for persona in dati:
+        if len(persona) > 0:  #ignorare dizionari vuoti
+            persona["SBAGLIATO"] = []
+
+            #Controllare tutte le chiave presenti per la persona: Se non sono nel gruppo lista_criteri, segna un errore nella lista "SBAGLIATO"
+            for chiave in persona:  
+                if chiave not in lista_criteri:
+                    persona["SBAGLIATO"].append(f'Dati non definiti presenti: "{chiave}" non consentito')
+
+            #Controllare per persone senza criteri e tipi sbagliati dei criteri
+            if "eta" not in persona:
+                persona["SBAGLIATO"].append('Manca "eta"')
+            elif type(persona["eta"]) != int:
+                persona["SBAGLIATO"].append('tipo del dato sbagliato: eta')
+
+            if "nome" not in persona:
+                persona["SBAGLIATO"].append('Manca "nome"')
+            elif type(persona["nome"]) != str:
+                persona["SBAGLIATO"].append('tipo del dato sbagliato: nome')
+
+            if "citta" not in persona:
+                persona["SBAGLIATO"].append('Manca "citta"')
+            elif type(persona["citta"]) != str:
+                persona["SBAGLIATO"].append('tipo del dato sbagliato: citta')
+
+            #Finalmente raccogliere tutte le persone con dati problematici
+            if len(persona["SBAGLIATO"]) > 0:
+                lista_sbagliati.append(persona)
+
+    #Stampa dei problemi, se ci sono:
+    if len(lista_sbagliati) > 0:
+        print(f"{len(lista_sbagliati)} problemi trovati: queste persone hanno i seguenti problemi:")
+        for persona in lista_sbagliati:
+            print(persona)
+    else: 
+        print("NESSUN PROBLEMA CON DATI")
+
+        
 if __name__ == "__main__":
     # Codice per provare la funzione "elabora_dati_json"
-    pass
+    elabora_dati_json(leggi_da_file("../dati/test.json"))
 
